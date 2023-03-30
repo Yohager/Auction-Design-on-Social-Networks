47b4c6dbc34580e575e8dae651492fe3d7eb2fc7
郭宇航 <yohager@guoyuhangdeMac-mini.local>
2023-03-31 01:03:04 +0800

    update rooted tree simulation results

--- IDM/idm_verify.py
+++ IDM/idm_verify.py
@@ -213,8 +213,6 @@ class IDMVerify:
         return winner, rewarded_bidders, rev 
 
 
-
-    
     def IDMRev(self, vals):
         '''
         calculate revenue of IDM directly: Rev = SW*(N\d1)
@@ -317,83 +315,100 @@ def plot_func(x, y_lists):
     colors = ['red', 'yellow', 'blue', 'green', \
               'purple', 'orange', 'cyan', 'grey',\
                 'lightblue', 'pink', 'tan']
-    y_lists_smooth = []
+    # y_lists中包含四幅图 
+    y_lists_rps, y_lists_rev = [], []
     for y in y_lists:
+        y_lists_rps.append([x[0] for x in y])
+        y_lists_rev.append([x[1] for x in y])
+    # print(y_lists_rev)
+    y_lists_smooth = []
+    for y in y_lists_rev:
         tmp = gaussian_filter1d(y, sigma = 10)
         y_lists_smooth.append(tmp)
+    print('here', y_lists_smooth)
     if len(colors) < len(y_lists_smooth):
         return 'Colors not enough!'
     n = len(y_lists_smooth)
     legends = ['Tree-' + str(i+1) for i in range(n)]
     selected_colors = colors[:n]
     for y, c in zip(y_lists_smooth, selected_colors):
+        print('debug', x, y)
         plt.plot(x, y, color = c)
-    plt.xlabel('reserved price')
-    plt.ylabel('revenue')
+    # 找到最大的值的位置并将点标出来
+    max_x, max_y = [], []
+    for i, y in enumerate(y_lists_smooth):
+        idx = np.argmax(y)
+        max_x.append(y_lists_rps[i][idx])
+        max_y.append(max(y))
+    # plt.scatter(max_x, max_y, marker = 'd', markersize = 20, color = 'black')
+    for dx, dy in zip(max_x, max_y):
+        plt.plot([dx], [dy], marker='d', markersize=20, color='black')
+    plt.xticks(fontsize=18)
+    plt.yticks(fontsize=18)
+    plt.xlabel('reserved price', fontsize=18)
+    plt.ylabel('revenue', fontsize=18)
     # plt.legend(('T0', 'T1', 'T2', 'T3', 'T4'))
-    plt.legend(legends)
-    plt.savefig('rooted_trees_with_4_bidders_smooth.png')
+    plt.legend(legends, fontsize=18)
+    plt.tight_layout()
+    plt.savefig('rooted_trees_with_4_bidders_smooth.pdf')
     plt.show()
 
+def trees_with_three_bidders(n, edges_sets):
+    nodes = [0,1,2,3]
+    G_list = []
+    for i in range(n):
+        tmp_G = nx.Graph()
+        tmp_G.add_nodes_from(nodes)
+        tmp_G.add_edges_from(edges_sets[i])
+        G_list.append(tmp_G)
+    # G0.add_edges_from()
+    return G_list 
 
-def trees_with_four_nodes():
-    G0 = nx.Graph()
+def trees_with_four_bidders(n, edges_sets):
     nodes = [0,1,2,3,4]
-    G0.add_nodes_from(nodes)
-    G0.add_edges_from([(0,1), (0,2), (0,3), (0,4)])
-    G1 = nx.Graph()
-    G1.add_nodes_from(nodes)
-    G1.add_edges_from([(0,1), (0,2), (0,3), (1,4)])
-    G2 = nx.Graph()
-    G2.add_nodes_from(nodes)
-    G2.add_edges_from([(0,1), (0,2), (1,3), (2,4)])
-    G3 = nx.Graph()
-    G3.add_nodes_from(nodes)
-    G3.add_edges_from([(0,1), (0,2), (2,3), (3,4)])
-    G4 = nx.Graph()
-    G4.add_nodes_from(nodes)
-    G4.add_edges_from([(0,1), (1,2), (2,3), (3,4)])
-    return list(G0, G1, G2, G3, G4)
+    G_list = []
+    for i in range(n):
+        tmp_G = nx.Graph()
+        tmp_G.add_nodes_from(nodes)
+        tmp_G.add_edges_from(edges_sets[i])
+        G_list.append(tmp_G)
+    # G0.add_edges_from()
+    return G_list
 
-def trees_with_five_nodes():
-    pass 
-    # nodes = [0, 1, 2, 3, 4, 5] 
-    # G0 = nx.Graph()
-    # G0.add_nodes_from(nodes)
-    # G0.add_edges_from([(0,1), (0,2), (0,3), (0,4), (0.5)])
-    # G1 = nx.Graph()
 
 def calc_results(Graph_list, seller_id, buyer_ids, x_vals, iters):
     y_lists = []
     for G in Graph_list:
         tmp_idm = IDMVerify(G, seller_id, buyer_ids)
         tmp_res = tmp_idm.test_rp(x_vals, iters)
-        plot_res = [x[1] for x in tmp_res]
-        y_lists.append(plot_res)
+        # plot_res = [x[1] for x in tmp_res]
+        y_lists.append(tmp_res)
     return y_lists
 
 if __name__ == "__main__":
-    G0, G1, G2, G3, G4 = trees_with_four_nodes()
     seller_id = 0
+    # buyer_ids = [1,2,3]
     buyer_ids = [1,2,3,4]
-    iters = 20000
+    # edges_sets = [
+    #     [(0,1), (1,2), (2,3)],
+    #     [(0,1), (1,2), (1,3)],
+    #     [(0,1), (0,2), (1,3)], 
+    #     [(0,1), (0,2), (0,3)]
+    # ]
+    edges_sets = [
+        [(0,1), (1,2), (2,3), (3,4)],
+        [(0,1), (1,2), (2,3), (2,4)],
+        [(0,1), (1,2), (1,3), (2,4)],
+        [(0,1), (1,2), (1,3), (1,4)],
+        [(0,1), (0,2), (1,3), (1,4)],
+        [(0,1), (0,2), (1,3), (2,4)],
+        [(0,1), (0,2), (1,3), (3,4)],
+        [(0,1), (0,2), (0,3), (1,4)],
+        [(0,1), (0,2), (0,3), (0,4)]
+    ]
+    iters = 10000 
+    # x_ranges = [0.01 * i for i in range(40, 81)]
     x_ranges = [0.002 * i for i in range(200, 401)]
-    IDM_test0 = IDMVerify(G0, seller_id, buyer_ids)
-    # print(IDM_test.main(1000))
-    IDM_res0 = IDM_test0.test_rp(x_ranges, iters)
-    IDM_test1 = IDMVerify(G1, seller_id, buyer_ids)
-    # print(IDM_test.main(1000))
-    IDM_res1 = IDM_test1.test_rp(x_ranges, iters)
-    IDM_test2 = IDMVerify(G2, seller_id, buyer_ids)
-    IDM_res2 = IDM_test2.test_rp(x_ranges, iters)
-    IDM_test3 = IDMVerify(G3, seller_id, buyer_ids)
-    IDM_res3 = IDM_test3.test_rp(x_ranges, iters)
-    IDM_test4 = IDMVerify(G4, seller_id, buyer_ids)
-    IDM_res4 = IDM_test4.test_rp(x_ranges, iters) 
-    # plot_x = [0.005 * i for i in range(60, 201)]
-    plot_y0 = [x[1] for x in IDM_res0]
-    plot_y1 = [x[1] for x in IDM_res1]
-    plot_y2 = [x[1] for x in IDM_res2]
-    plot_y3 = [x[1] for x in IDM_res3]
-    plot_y4 = [x[1] for x in IDM_res4]
-    plot_func(x_ranges, plot_y0, plot_y1, plot_y2, plot_y3, plot_y4)
+    G_list = trees_with_four_bidders(9, edges_sets)
+    y_res = calc_results(G_list, seller_id, buyer_ids, x_ranges, iters)
+    plot_func(x_ranges, y_res)
